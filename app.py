c0acabc8b3cc2fc0eeec028a8bca4c9beca34ba8
phamlong666 <phamlong666@gmail.com>
2025-06-12 09:54:33 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ with col1:
     try:
         logo = Image.open("assets/logo_hinh_tron_hoan_thien.png")
         st.image(logo, width=70)
-    except:
-        pass
+    except Exception as e:
+        st.warning("Không tìm thấy logo.")
 except:
 st.warning("⚠️ Không tìm thấy logo.")
 with col2:
