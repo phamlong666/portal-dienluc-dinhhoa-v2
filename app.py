807f498a0dc5da8bb39072c3238e833d97c36274
phamlong666 <phamlong666@gmail.com>
2025-06-19 14:20:31 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -377,15 +377,7 @@ elif chon_modul == '📍 Dự báo điểm sự cố':
         except Exception as e:
             st.warning(f"⚠️ Không thể đọc file: {e}")
     
-if "suco_data" not in st.session_state:
-    try:
-        if os.path.exists("du_lieu_su_co.xlsx"):
-            df_uploaded = pd.read_excel("du_lieu_su_co.xlsx")
-            st.session_state.suco_data = df_uploaded.to_dict(orient="records")
-        else:
-            st.session_state.suco_data = []
-    except:
-        st.session_state.suco_data = []
+    if "suco_data" not in st.session_state:
         st.session_state.suco_data = []
     
     with st.form("suco_form"):
